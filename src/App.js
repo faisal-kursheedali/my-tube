26fda54b3ff084d919f37940e29c176a82c463a4
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-08-10 18:47:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
-import {useEffect} from "react"
-import { useAuth } from "./frontend/context/authContext";
-import { SET_STATUS_TRUE, USER_TOKEN } from "./frontend/constant/authConstant";
-import { getLocalStorage } from "./frontend/utility/localStorage";
-import { getLikedVdo } from "./frontend/services/likedVdoData";
-import { getHistory } from "./frontend/services/historyData";
-import { getWatchlater } from "./frontend/services/watchlaterData";
+// import {useEffect} from "react"
+// import { useAuth } from "./frontend/context/authContext";
+// import { SET_STATUS_TRUE, USER_TOKEN } from "./frontend/constant/authConstant";
+// import { getLocalStorage } from "./frontend/utility/localStorage";
+// import { getLikedVdo } from "./frontend/services/likedVdoData";
+// import { getHistory } from "./frontend/services/historyData";
+// import { getWatchlater } from "./frontend/services/watchlaterData";
 import PgRoute from "./frontend/routes/pgRoute";
 
 
 function App() {
-  const token = getLocalStorage("token");
-  const {authDispatch}=useAuth();
-  useEffect(() => {
-    if (token) {
-      authDispatch({
-        type:USER_TOKEN,
-        payload:token
-      });
-      authDispatch({
-        type:SET_STATUS_TRUE
-      })
-      getLikedVdo();
-      getHistory();
-      getWatchlater();
-    }
-  }, [token]);
+  // const token = getLocalStorage("token");
+  // const {authDispatch}=useAuth();
+  // useEffect(() => {
+  //   if (token) {
+  //     authDispatch({
+  //       type:USER_TOKEN,
+  //       payload:token
+  //     });
+  //     authDispatch({
+  //       type:SET_STATUS_TRUE
+  //     })
+  //     getLikedVdo();
+  //     getHistory();
+  //     getWatchlater();
+  //   }
+  // }, [token]);
   return (
     <div className="App">
       <PgRoute/>
