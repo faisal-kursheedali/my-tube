3601a376120c68c23243f57f8d4630fbec3ecd1d
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-08-29 08:57:44 +0530

    made auth ui

--- src/frontend/pages/login.jsx
+++ src/frontend/pages/login.jsx
@@ -1,58 +1,51 @@
-import React,{useState} from 'react'
+import React from 'react'
 import "./css/login.css"
 import {Link} from "react-router-dom"
 import { useAuth } from '../context/authContext'
 import { loginUser } from '../services/authServices'
 import { useNavigate,useLocation } from "react-router-dom";
 import { USER_EMAIL, USER_PASSWORD } from '../constant/authConstant';
+import "./css/auth.css"
 
 const Login = () => {
   const location=useLocation();
   const navigate=useNavigate();
-  const [click, setClick] = useState(false);
   const{authState,authDispatch}=useAuth();
   const navigateFnc=()=>{
     navigate(location.state.from.pathname,{replace:true});
   }
   return (
     <>
-    {/* {
-      !click?
-      <div className="login-container">
-      <div className="login">
-      <div className="login-box">
-          <div className="login-txt-head">Youtube clone</div>
-          <img src="https://developers.google.com/site-assets/logo-youtube.svg" alt="" className="login-image" />
-          <p className="login-text">Enjoy the vedio by logging in</p>
-          <button className="login-btn bui-btn bui-btn-sm" onClick={()=>setClick(true)}>Login with Google</button>
+    <div className="auth-containe">
+      <div className="auth-head">
+        <div className="auth-head-img">
+        <img src="https://developers.google.com/site-assets/logo-youtube.svg" alt="" className="auth-head-img" />
+        <div className="auth-head-txt">MY Tube</div>
+        </div>
       </div>
-      </div>
-      </div>:""
-    } */}
-    
-    {
-      click?(<>
-      <div>Login</div>
-    {/* <input type="text" placeholder='name' onChange={(e)=>authState.name=e.target.value} value={authState.name}/> */}
-    <input type="email" placeholder='email' onChange={(e)=>authDispatch({
+      <div className="auth-form">
+
+      <div className='auth-form-head-txt'>login</div>
+        <div className="auth-form-sub-txt">Enjoy all the video for free</div>
+    <input type="email" className='auth-input' placeholder='email' onChange={(e)=>authDispatch({
       type:USER_EMAIL,
       payload:e.target.value
     })} value={authState.email} />
-    <input type="password" placeholder='password' onChange={(e)=>authDispatch({
+    <input type="password" className='auth-input' placeholder='password' onChange={(e)=>authDispatch({
       type:USER_PASSWORD,
       payload:e.target.value
     })} value={authState.password} />
     <button onClick={()=>{
       
       loginUser(authState,authDispatch,navigateFnc)
-      setClick(false);
-    }}>LOGIN</button>
-    <Link to={"/signup"}>Signup</Link>
       
-      </>):""
-    }
-    
+    }} className="auth-btn">LOGIN</button>
+    <Link to={"/signup"} className="auth-link">signup</Link> 
+    </div>
+    </div> 
+      
     </>
+      
   )
 }
 
--- src/frontend/pages/signup.jsx
+++ src/frontend/pages/signup.jsx
@@ -1,25 +1,24 @@
-import React,{useState} from 'react'
+import React from 'react'
 import { useAuth } from '../context/authContext'
 import {  signupUser } from '../services/authServices'
 import { useNavigate} from "react-router-dom";
 import { USER_EMAIL, USER_NAME, USER_PASSWORD } from '../constant/authConstant';
+import {Link} from "react-router-dom"
 
-import "./css/signup.css"
+import "./css/auth.css"
 
 const Signup = () => {
   // const location=useLocation();
   const navigate=useNavigate();
   
-  const [click, setClick] = useState(false);
   const{authState,authDispatch}=useAuth();
   const navigateFnc=()=>{
     navigate("/",{replace:true});
   }
   return (
     <>
-    {
-      !click?
-      <div className="login-container">
+    
+      {/* <div className="login-container">
       <div className="login">
       <div className="login-box">
           <div className="login-txt-head">Youtube clone</div>
@@ -28,30 +27,39 @@ const Signup = () => {
           <button className="login-btn bui-btn bui-btn-sm" onClick={()=>setClick(true)}>Signup with Google</button>
       </div>
       </div>
-      </div>:""
-    }
-    {
-      click?( <>
-      <div>signup</div>
-    <input type="text" placeholder='name' onChange={(e)=>authDispatch({
+      </div>:"" */}
+    
+     <>
+     <div className="auth-containe">
+      <div className="auth-head">
+        <div className="auth-head-img">
+        <img src="https://developers.google.com/site-assets/logo-youtube.svg" alt="" className="auth-head-img" />
+        <div className="auth-head-txt">MY Tube</div>
+        </div>
+      </div>
+      <div className="auth-form">
+
+      <div className='auth-form-head-txt'>signup</div>
+        <div className="auth-form-sub-txt">Enjoy all the video for free</div>
+    <input type="text" className='auth-input' placeholder='name' onChange={(e)=>authDispatch({
       type:USER_NAME,
       payload:e.target.value
     })} value={authState.name}/>
-    <input type="email" placeholder='email' onChange={(e)=>authDispatch({
+    <input type="email" className='auth-input' placeholder='email' onChange={(e)=>authDispatch({
       type:USER_EMAIL,
       payload:e.target.value
     })} value={authState.email} />
-    <input type="password" placeholder='password' onChange={(e)=>authDispatch({
+    <input type="password" className='auth-input' placeholder='password' onChange={(e)=>authDispatch({
       type:USER_PASSWORD,
       payload:e.target.value
     })} value={authState.password} />
     <button onClick={()=>{
-      signupUser(authState,authDispatch,navigateFnc);
-      setClick(false)
-    }}>LOGIN</button>
-      
-      </>):""
-    }
+      signupUser(authState,authDispatch,navigateFnc); 
+    }} className="auth-btn">sign up</button>
+    </div>
+    <Link to={"/login"} className="auth-link">login</Link> 
+    </div> 
+      </>
     
     </>
   )
